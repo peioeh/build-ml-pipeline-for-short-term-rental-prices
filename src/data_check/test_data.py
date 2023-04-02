d312c50723009207fcffb49a3541786639b51587
Peio Lahirigoyen <PeioLahirigoyen@fico.com>
2023-04-02 14:35:11 +0200

    data_check code

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    """
+    Check number of rows
+    """
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_price_range(data, min_price, max_price):
+    """
+    Check the range of prices. They should be between min_price and max_price
+    """
+    idx = data['price'].between(min_price, max_price)
+    assert np.sum(~idx) == 0
